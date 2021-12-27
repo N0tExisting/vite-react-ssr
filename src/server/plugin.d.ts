81ab7ee17152516607c5b4e002a016c0c75b6203
Not Existing <61290107+N0tExisting@users.noreply.github.com>
2021-12-27 15:21:05 +0100

    chore: remove unnecesary type imports

--- src/server/plugin.d.ts
+++ src/server/plugin.d.ts
@@ -1,10 +1,3 @@
-import type {
-	FastifyLoggerInstance,
-	RawReplyDefaultExpression,
-	RawRequestDefaultExpression,
-	RawServerBase,
-	RawServerDefault,
-} from 'fastify';
 import type { ViteDevServer } from 'vite';
 
 declare module 'fastify/types/instance' {
